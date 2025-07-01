ffc0b926eb5f93a31be510faee6d9ccc0dda67bd
Mohammed Alkhayat <46660096+devalkhayat@users.noreply.github.com>
2025-07-01 04:03:59 +0300

    fix issues

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -3,7 +3,7 @@ import 'constants.dart';
 const String fontFamilyName="roboto";
 const String appName="Movies Explorer";
 const String baseUrl="https://api.themoviedb.org/3/";
-const String token="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzQ5ODBhOTVmODM5ZDZkZmJhNzQ2ZmNmN2RiNjY4OSIsIm5iZiI6MTcyMTIxNTMyNC41MDg5OTk4LCJzdWIiOiI2Njk3YTk1YzY1NDYzYzBmMjA2NmRiZjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.pb3CzHqzWCosXHyZUQsvarYl4ndiX_BHI6BSQRIE5es";
+const String token="";
 const String hiveDatabase="database";
 const String basicImageUrl="https://image.tmdb.org/t/p/original";
 
